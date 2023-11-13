1e9d665661ea26de73d7b718c10819ad375c0b59
Warren Fernandes <warren@strata.io>
2023-11-13 12:55:20 -0700

    Update Header Creation SE with v2 service extension format
    Signed-off-by: Warren Fernandes <warren@strata.io>

---
+++ header-creation/go.mod
@@ -0,0 +1,5 @@
+module github.com/strata-io/strata-service-extension-examples/header-creation
+
+go 1.20
+
+require github.com/strata-io/service-extension v0.4.0
--- header-creation/header.go
+++ header-creation/header.go
@@ -1,24 +1,33 @@
 package main
 
 import (
-	"maverics/app"
-	"maverics/log"
-	"maverics/session"
-
+	"fmt"
 	"net/http"
+
+	"github.com/strata-io/service-extension/orchestrator"
 )
 
 // CreateFirstNameHeader creates a custom first name header. The user's first name
 // will be concatenated with "the Great".
 func CreateFirstNameHeader(
-	ag *app.AppGateway,
-	rw http.ResponseWriter,
-	req *http.Request,
+	api orchestrator.Orchestrator,
+	_ http.ResponseWriter,
+	_ *http.Request,
 ) (http.Header, error) {
-	log.Debug("msg", "retrieving first name from session")
-	name := session.GetString(req, "azure.given_name") + " the Great"
+	logger := api.Logger()
+	logger.Info("se", "building first name custom header")
 
-	log.Debug("msg", "building custom first name header")
+	session, err := api.Session()
+	if err != nil {
+		return nil, fmt.Errorf("unable to retrieve session: %w", err)
+	}
+	logger.Debug("se", "retrieving first name from session")
+	firstName, err := session.GetString("azure.given_name")
+	if err != nil {
+		return nil, fmt.Errorf("unable to retrieve attribute 'azure.given_name': %w", err)
+	}
+	name := firstName + " the Great"
+
 	header := make(http.Header)
 	header["EXAMPLE-FIRST-NAME"] = []string{name}
 	return header, nil
@@ -27,15 +36,24 @@ func CreateFirstNameHeader(
 // CreateLastNameHeader creates a custom last name header. The user's last name
 // will be prepended with "Dr.".
 func CreateLastNameHeader(
-	ag *app.AppGateway,
-	rw http.ResponseWriter,
-	req *http.Request,
+	api orchestrator.Orchestrator,
+	_ http.ResponseWriter,
+	_ *http.Request,
 ) (http.Header, error) {
-	log.Debug("msg", "retrieving last name from session")
-	lastName := "Dr. " + session.GetString(req, "azure.family_name")
+	logger := api.Logger()
+	logger.Info("se", "building last name custom header")
 
-	log.Debug("msg", "building custom last name header")
+	session, err := api.Session()
+	if err != nil {
+		return nil, fmt.Errorf("unable to retrieve session: %w", err)
+	}
+	lastName, err := session.GetString("azure.family_name")
+	if err != nil {
+		return nil, fmt.Errorf("unable to retrieve attribute 'azure.family_name': %w", err)
+	}
+
+	nameWithTitle := "Dr. " + lastName
 	header := make(http.Header)
-	header["EXAMPLE-LAST-NAME"] = []string{lastName}
+	header["EXAMPLE-LAST-NAME"] = []string{nameWithTitle}
 	return header, nil
 }
