84f4e670d9f3b6df325f732b66d982a2ad886003
Warren Fernandes <warren@strata.io>
2023-11-15 10:32:11 -0700

    Update LDAP TLS SE with v2 service extension format (#33)
    * Update LDAP TLS SE with v2 service extension format
    
    ---------
    
    Signed-off-by: Warren Fernandes <warren@strata.io>

--- ldap-tls/auth.go
+++ ldap-tls/auth.go
@@ -3,30 +3,35 @@ package main
 import (
 	"crypto/tls"
 	"crypto/x509"
-	"errors"
 	"fmt"
 	"net/http"
 
-	"maverics/app"
-	"maverics/ldap"
-	"maverics/log"
-	"maverics/secret"
-	"maverics/session"
-)
-
-const (
-	idpName          = "ldap"
-	ldapURL          = "ldap://ldap.examples.com"
-	ldapServerName   = "ldap.examples.com"
-	ldapBaseDN       = "ou=People,dc=examples,dc=com"
-	ldapCASecretName = "ldapCA"
+	ldap3 "github.com/go-ldap/ldap/v3"
+	"github.com/strata-io/service-extension/orchestrator"
 )
 
 // IsAuthenticated determines if the user has been authenticated.
-func IsAuthenticated(ag *app.AppGateway, rw http.ResponseWriter, req *http.Request) bool {
-	log.Debug("se", "determining if user is authenticated")
+func IsAuthenticated(api orchestrator.Orchestrator, _ http.ResponseWriter, _ *http.Request) bool {
+	logger := api.Logger()
+	session, err := api.Session()
+	if err != nil {
+		logger.Error("se", "unable to retrieve session", "error", err.Error())
+		return false
+	}
+	metadata := api.Metadata()
+	idpName := metadata["idpName"].(string)
 
-	if session.GetString(req, fmt.Sprintf("%s.authenticated", idpName)) == "true" {
+	logger.Debug("se", "determining if user is authenticated")
+
+	isAuthenticated, err := session.GetString(fmt.Sprintf("%s.authenticated", idpName))
+	if err != nil {
+		logger.Error(
+			"se", fmt.Sprintf("unable to retrieve session value '%s.authenticated'", idpName),
+			"error", err.Error(),
+		)
+		return false
+	}
+	if isAuthenticated == "true" {
 		return true
 	}
 
@@ -34,45 +39,78 @@ func IsAuthenticated(ag *app.AppGateway, rw http.ResponseWriter, req *http.Reque
 }
 
 // Authenticate authenticates the user against the IDP.
-func Authenticate(ag *app.AppGateway, rw http.ResponseWriter, req *http.Request) error {
+func Authenticate(api orchestrator.Orchestrator, rw http.ResponseWriter, req *http.Request) {
+	logger := api.Logger()
+	session, err := api.Session()
+	if err != nil {
+		logger.Error("se", "unable to retrieve session", "error", err.Error())
+		http.Error(rw, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+		return
+	}
+	secretProvider, err := api.SecretProvider()
+	if err != nil {
+		logger.Error("se", "unable to retrieve secret provider", "error", err.Error())
+		http.Error(rw, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+		return
+	}
+	metadata := api.Metadata()
+	idpName := metadata["idpName"].(string)
+	ldapURL := metadata["ldapURL"].(string)
+	ldapServerName := metadata["ldapServerName"].(string)
+	ldapBaseDN := metadata["ldapBaseDN"].(string)
+	ldapCASecretName := metadata["ldapCASecretName"].(string)
+
 	username, password, ok := req.BasicAuth()
 	if !ok {
-		log.Error("se", "unable to read basic auth headers")
-		return errors.New("unable to read basic auth headers")
+		logger.Error("se", "unable to read basic auth headers")
+		http.Error(rw, http.StatusText(http.StatusBadRequest), http.StatusBadRequest)
+		return
 	}
 
-	log.Info(
+	logger.Info(
 		"se", "logging user in",
 		"req", req.URL.String(),
 		"username", username,
 	)
 	certPool, err := x509.SystemCertPool()
 	if err != nil {
-		return fmt.Errorf("unable to load system cert pool: %w", err)
+		logger.Error("se", "unable to load system cert pool", "error", err.Error())
+		http.Error(rw, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+		return
 	}
-	ldapCACert := secret.GetString(ldapCASecretName)
+	ldapCACert := secretProvider.GetString(ldapCASecretName)
 	certPool.AppendCertsFromPEM([]byte(ldapCACert))
 	tlsCfg := &tls.Config{
 		ServerName: ldapServerName,
 		RootCAs:    certPool,
 	}
 
-	conn, err := ldap.DialURL(ldapURL, ldap.DialWithTLSConfig(tlsCfg))
+	conn, err := ldap3.DialURL(ldapURL, ldap3.DialWithTLSConfig(tlsCfg))
 	if err != nil {
-		return fmt.Errorf("unable to do ldap dial: %w", err)
+		logger.Error("se", "unable to do ldap dial", "error", err.Error())
+		http.Error(rw, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+		return
 	}
 	defer conn.Close()
 
 	usernameKey := fmt.Sprintf("uid=%s,%s", username, ldapBaseDN)
 	err = conn.Bind(usernameKey, password)
 	if err != nil {
-		return fmt.Errorf("unable to do ldap bind: %w", err)
+		logger.Error("se", "unable to do ldap bind", "error", err.Error())
+		http.Error(rw, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+		return
 	}
-	session.Set(req, fmt.Sprintf("%s.authenticated", idpName), "true")
-	session.Set(req, fmt.Sprintf("%s.cn", idpName), username)
-	log.Info("se", "user authenticated", "username", username)
-	return nil
 
-	log.Info("se", "successfully authenticated", "username", username)
-	return nil
+	_ = session.SetString(fmt.Sprintf("%s.authenticated", idpName), "true")
+	_ = session.SetString(fmt.Sprintf("%s.cn", idpName), username)
+
+	err = session.Save()
+	if err != nil {
+		logger.Error("se", "unable to save session", "error", err.Error())
+		http.Error(rw, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+		return
+	}
+
+	logger.Info("se", "user successfully authenticated", "username", username)
+	return
 }
---
+++ ldap-tls/go.mod
@@ -0,0 +1,15 @@
+module github.com/strata-io/strata-service-extension-examples/ldap-tls
+
+go 1.20
+
+require (
+	github.com/go-ldap/ldap/v3 v3.4.6
+	github.com/strata-io/service-extension v0.4.0
+)
+
+require (
+	github.com/Azure/go-ntlmssp v0.0.0-20221128193559-754e69321358 // indirect
+	github.com/go-asn1-ber/asn1-ber v1.5.5 // indirect
+	github.com/google/uuid v1.3.1 // indirect
+	golang.org/x/crypto v0.13.0 // indirect
+)
